a28f41b6318f0b3d03bc8984044ad8ebfd9fa842
Enda Lee <enda.lee@tudublin.ie>
2022-10-16 20:34:33 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+    </nav>
+    <!-- End Nav -->
+
+    <slot />
